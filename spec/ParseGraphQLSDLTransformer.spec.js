2cf9887109299d67e0cecfed1f0444c4d3750141
Antoine Cormouls <contact.antoine.cormouls@gmail.com>
2019-08-18 00:51:53 +0200

    wip

---
+++ spec/ParseGraphQLSDLTransformer.spec.js
@@ -0,0 +1,54 @@
+const { SDLTransformer } = require('../lib/GraphQL/transformers/SDL');
+
+describe('GraphQLSDLToParseSchema', () => {
+  let parseServer;
+
+  beforeAll(async () => {
+    parseServer = await global.reconfigureServer({});
+  });
+
+  it('should add new fields when not exist in Parse.Schema', async () => {
+    await (new Parse.Object("Customer")).save()
+    const typeDefs = `
+      type User {
+        name: String
+      }
+
+      type Team {
+        name: String
+      }
+
+      type Company {
+        name: String
+      }
+    `
+    SDLTransformer(parseServer, typeDefs)
+  });
+
+  it('should remove fields in Parse.Schema when not exist in SDL', () => {
+  });
+
+  it('should support all fields types', () => {
+  });
+
+  it('should support type User', () => {
+  });
+
+  it('should support type Role', () => {
+  });
+
+  it('should throw an error when try to modifiy type of an existing field', () => {
+  });
+
+  it('should support index directive', () => {
+  });
+
+  it('should add index when not detected in SDL', () => {
+  });
+
+  it('should remove index when not detected in SDL', () => {
+  });
+
+  it('should throw an error when try to modifiy type of an existing field', () => {
+  });
+});
---
+++ src/GraphQL/transformers/SDL.js
@@ -0,0 +1,50 @@
+import { buildSchema } from 'graphql';
+import { systemClasses, defaultColumns } from '../../Controllers/SchemaController'
+import { transformClassNameToGraphQL } from './className'
+
+const RESERVED_NATIVE_TYPE = [
+  '__Schema',
+  '__Type',
+  '__TypeKind',
+  'String',
+  'Boolean',
+  'Float',
+  'Int',
+  'Mutation',
+  'Query',
+  'Subscription',
+  '__Field',
+  '__InputValue',
+  '__EnumValue',
+  '__Directive',
+  '__DirectiveLocation'
+]
+
+// Transform a GraphQL SDL Schemma to Parse.Schema
+const SDLTransformer = async (parseServer, typeDefs) => {
+
+  const SDLSchema = buildSchema(typeDefs)
+  const types = Object.keys(SDLSchema._typeMap).filter(type => !RESERVED_NATIVE_TYPE.includes(type))
+  const parseSchema = await parseServer.config.databaseController.loadSchema()
+  const classes = await parseSchema.getAllClasses()
+
+  await deleteClasses(classes, types, parseServer.config.databaseController)
+
+  console.log(SDLSchema._typeMap.Company._fields)
+}
+
+const deleteClasses = async (classes, types, databaseController) => {
+  const classesToDelete = classes.filter(clazz => !systemClasses.includes(clazz.className) && !types.includes(transformClassNameToGraphQL(clazz.className)))
+  await Promise.all(classesToDelete.map(async (className) => databaseController.deleteSchema(className)))
+}
+
+const createOrUpdateClass = async (clazz, type) => {
+  const typeFields = Object.keys(type._fields).map(fieldName => ({name: fieldName, type: JSON.stringify(type._fields[type._fields].type)}))
+  console.log(typeFields)
+}
+
+// addClassIfNotExists(className, fields)
+// updateClass(className: string,submittedFields: SchemaFields,classLevelPermissions: any,indexes: any,database: DatabaseController)
+// cofing.databaseController.deleteSchema(className)
+
+export { SDLTransformer }
